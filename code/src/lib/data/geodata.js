a22b648607cc86f516d6f612d87a06a0d7661e13
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2024-04-03 22:15:59 -0400

    working on first map visualization

---
+++ code/src/lib/data/geodata.js
@@ -0,0 +1,130 @@
+export const greatcircles = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [[51.175263, 25.358905], [-58.387367, -34.567209], [-58.380193, -34.525014]]
+        },
+        "properties": {}
+    }, {
+        "type": "Feature",
+        "geometry": {"type": "LineString", "coordinates": [[51.176791, 25.362025], [151.19597, -33.942726]]},
+        "properties": {}
+    }, {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {"type": "LineString", "coordinates": [[51.175263, 25.3599], [158.616866, 53.000134]]}
+    }, {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {"type": "LineString", "coordinates": [[51.178211, 25.361977], [-71.198265, 46.814412]]}
+    }]
+};
+
+export const ports = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [121.654869, 31.327003]},
+            "properties": {"en": "Shanghai"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [103.69785, 1.277471]},
+            "properties": {"en": "Singapore"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [113.885504, 22.491196]},
+            "properties": {"en": "Shenzen"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [122.146635, 29.958487]},
+            "properties": {"en": "Ningbo-Zhoushan"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [113.449026, 23.095017]},
+            "properties": {"en": "Guangzhou"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [129.079073, 35.106376]},
+            "properties": {"en": "Busan"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [114.128716, 22.333083]},
+            "properties": {"en": "Hong Kong"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [120.322848, 36.097143]},
+            "properties": {"en": "Qingdao"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [117.769811, 38.911834]},
+            "properties": {"en": "Tianjin"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [55.067156, 24.992933]},
+            "properties": {"en": "Dubai"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [4.021466, 51.104636]},
+            "properties": {"en": "Antwerp"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [4.428523, 51.893937]},
+            "properties": {"en": "Rotterdam"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [101.297007, 2.935967]},
+            "properties": {"en": "Kelang"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-118.217209, 33.7539344]},
+            "properties": {"en": "Long Beach"}
+        },
+        {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [118.057571, 24.408561]},
+            "properties": {"en": "Xiamen"}
+        }
+    ]
+}
+
+export const routes = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [[51.797112, 25.65821], [54.987332, 25.935534], [56.505437, 26.606314], [56.945467, 25.65821], [59.871669, 23.840078], [65.316172, 11.683018], [78.443404, -10.29584], [98.134252, -27.621735], [109.691924, -29.003481], [109.691924, -28.878613]]
+        },
+        "properties": {}
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [[62.177235, 18.779597], [50.087165, 12.746023], [44.512565, 11.38319], [36.642542, 24.37865], [32.522738, 29.510574], [30.651666, 33.939029], [21.04935, 34.809307], [13.494188, 35.952718], [10.960721, 38.048037], [6.979558, 38.580483], [4.174647, 42.130045], [4.129407, 42.130045]]
+        },
+        "properties": {}
+    }, {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [[65.287153, 11.759524], [62.206516, -7.372427], [58.594778, -25.545563], [49.291817, -36.226473], [37.58103, -38.573656], [30.576447, -37.191603], [28.168621, -34.62122], [28.152095, -34.563873], [28.264858, -34.470962]]
+        },
+        "properties": {}
+    }]
+}
