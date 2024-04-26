f28f4bea3340ee0846771c9ac5bfef41c6276065
amelia-b <amelia_b@mit.edu>
2024-04-26 14:23:31 -0400

    add census data

--- eda/generate_stop_zone_data.py
+++ eda/generate_stop_zone_data.py
@@ -5,6 +5,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import time
+
+start_time = time.time()
 
 PREPROCESSED_DATA_PATH = Path(
     "/Users/ameliabaum/Library/Mobile Documents/com~apple~CloudDocs/MIT/6.C35/6.C85_FP/data/preprocessed_data"
@@ -27,14 +30,21 @@ PREPROCESSED_DATA_PATH = Path(
 #                                  driver='GeoJSON')
 
 brookline_milton_parcels = gpd.read_file(PREPROCESSED_DATA_PATH /
-                                              'brookline_milton_parcels.geojson')
+                                         'brookline_milton_parcels.geojson')
 print(len(brookline_milton_parcels))
 
 # Assign zoning by parcel
 
 # these are for brookline, will need to see if they scale https://www.brooklinema.gov/DocumentCenter/View/19956/GIS---Zoning-Map-10CC-11x17pdf
-sf_zoning_codes = ['S-40', 'S-25', 'S-15', 'S-10', 'S-7', 'S-4', 'S-0.5P,' 'S-0.75P', 'SC-7', 'SC-10', 'T-6', 'T-5', 'SF', 'S10', 'S40', 'S25', 'S15', 'S7', 'S4']
-multifamily_zoning_codes = ['F10', 'M-0.5', 'M5', 'M10', 'M10(CAM)', 'M15', 'M20', 'M25'] #the zoning code says M1.0, M1.5, but the data has it has M10, M15, etc.
+#TODO: use some kind of fuzzy matching for these
+sf_zoning_codes = [
+    'S-40', 'S-25', 'S-15', 'S-10', 'S-7', 'S-4', 'S-0.5P,'
+    'S-0.75P', 'SC-7', 'SC-10', 'T-6', 'T-5', 'SF', 'S10', 'S40', 'S25', 'S15',
+    'S7', 'S4', 'R1'
+]
+multifamily_zoning_codes = [
+    'F10', 'M-0.5', 'M5', 'M10', 'M10(CAM)', 'M15', 'M20', 'M25'
+]  #the zoning code says M1.0, M1.5, but the data has it has M10, M15, etc.
 commercial_zoning_codes = [
     'L-0.5', 'L-0.5(CL)', 'L-1.0', 'G-1.0', 'G-(DP)', 'G-1.75(CC)',
     'G-1.75(LSH)', 'G-1.75(WS)', 'G-2.0', 'G-2.0(CA)', 'GMR-2.0', 'O-1.0',
@@ -43,55 +53,119 @@ commercial_zoning_codes = [
     'G175(WS)', 'G175(LSH', 'G10', 'GMR20', 'G(DP)', 'GRA', 'GRB'
 ]
 
-brookline_milton_parcels['isSFZoning'] = brookline_milton_parcels[
+brookline_milton_parcels['isZonedAsSF'] = brookline_milton_parcels[
     'ZONING'].apply(lambda zoning_code: zoning_code in sf_zoning_codes)
-brookline_milton_parcels['isMultiFamZoning'] = brookline_milton_parcels[
+brookline_milton_parcels['isZonedAsMultifamily'] = brookline_milton_parcels[
     'ZONING'].apply(
         lambda zoning_code: zoning_code in multifamily_zoning_codes)
-brookline_milton_parcels['isCommercialZoning'] = brookline_milton_parcels[
+brookline_milton_parcels['isZonedAsCommercial'] = brookline_milton_parcels[
     'ZONING'].apply(lambda zoning_code: zoning_code in commercial_zoning_codes)
 
 # Assign usage by parcel
 # these are for all of Massachusetts: https://www.cityofboston.gov/images_documents/ma_occcodes_tcm3-16189.pdf
 single_family_usage_codes = ['101']
-commercial_use_codes = [str(num) for num in range(310, 399 + 1)] # remove parking?
+commercial_use_codes = [str(num)
+                        for num in range(310, 399 + 1)]  # remove parking?
 duplex_use_codes = ['104']
 triplex_use_codes = ['105']
 multi_buildings_1lot_use_codes = ['109']
 apt_up_to_30_units_use_codes = ['111', '112']
 apt_over_30_units_use_codes = ['113', '114']
 coop_apt_use_codes = ['115']
-other_multi_family_use_codes = [string for string in [str(num) for num in range(106, 154 + 1)] if string not in ['104', '105', '109', '111', '112', '113', '114', '115']]
-
+other_multi_family_use_codes = [
+    string for string in [str(num) for num in range(106, 154 + 1)]
+    if string not in ['104', '105', '109', '111', '112', '113', '114', '115']
+]
 
 brookline_milton_parcels['isUsedAsSF'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in single_family_usage_codes)
-brookline_milton_parcels['isUsedAsComm'] = brookline_milton_parcels[
+brookline_milton_parcels['isUsedAsCommercial'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in commercial_use_codes)
 brookline_milton_parcels['isUsedAsDuplex'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in duplex_use_codes)
 brookline_milton_parcels['isUsedAsTriplex'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in triplex_use_codes)
-brookline_milton_parcels['isUsedAsMultiBuildings1Lot'] = brookline_milton_parcels[
-    'USE_CODE'].apply(lambda use_code: use_code in multi_buildings_1lot_use_codes)
+brookline_milton_parcels[
+    'isUsedAsMultiBuildings1Lot'] = brookline_milton_parcels['USE_CODE'].apply(
+        lambda use_code: use_code in multi_buildings_1lot_use_codes)
 brookline_milton_parcels['isUsedAsAptUpTo30Units'] = brookline_milton_parcels[
-    'USE_CODE'].apply(lambda use_code: use_code in apt_up_to_30_units_use_codes)
+    'USE_CODE'].apply(
+        lambda use_code: use_code in apt_up_to_30_units_use_codes)
 brookline_milton_parcels['isUsedAsAptOver30Units'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in apt_over_30_units_use_codes)
 brookline_milton_parcels['isUsedAsCoopApt'] = brookline_milton_parcels[
     'USE_CODE'].apply(lambda use_code: use_code in coop_apt_use_codes)
-brookline_milton_parcels['isUsedAsOtherMultifamily'] = brookline_milton_parcels[
-    'USE_CODE'].apply(lambda use_code: use_code in other_multi_family_use_codes)
+brookline_milton_parcels[
+    'isUsedAsOtherMultifamily'] = brookline_milton_parcels['USE_CODE'].apply(
+        lambda use_code: use_code in other_multi_family_use_codes)
 
-
-brookline_milton_parcels['mustUpzone'] = brookline_milton_parcels.apply(lambda row: True if row['isZonedAsSF'] else False, axis=1)
+brookline_milton_parcels['mustUpzone'] = brookline_milton_parcels.apply(
+    lambda row: True if row['isZonedAsSF'] else False, axis=1)
 brookline_milton_parcels['willChange'] = brookline_milton_parcels.apply(
     lambda row: True if (row['isZonedAsSF'] and row['isUsedAsSF']) else False,
     axis=1)
 
+
+def percentage_true(series):
+    return (series.sum() / len(series)) * 100
+
+
 # group parcels into stop zones by stop_name
+brookline_milton_stop_zone_data = brookline_milton_parcels.groupby(
+    'stop_name').agg({
+        'municipality': 'first',
+        'isZonedAsSF': percentage_true,
+        'isZonedAsCommercial': percentage_true,
+        'isZonedAsMultifamily': percentage_true,
+        'isUsedAsSF': percentage_true,
+        'isUsedAsCommercial': percentage_true,
+        'isUsedAsDuplex': percentage_true,
+        'isUsedAsTriplex': percentage_true,
+        'isUsedAsMultiBuildings1Lot': percentage_true,
+        'isUsedAsAptUpTo30Units': percentage_true,
+        'isUsedAsAptOver30Units': percentage_true,
+        'isUsedAsCoopApt': percentage_true,
+        'isUsedAsOtherMultifamily': percentage_true,
+        'mustUpzone': percentage_true,
+        'willChange': percentage_true,
+    }).reset_index()
+
+brookline_milton_stop_zone_data.rename(columns={
+    'isZonedAsSF': 'pctZonedAsSF',
+    'isZonedAsComm': 'pctZonedAsComm',
+    'isZonedAsMultifamily': 'pctZonedAsMultifamily',
+    'isUsedAsSF': 'pctUsedAsSF',
+    'isUsedAsCommercial': 'pctUsedAsCommercial',
+    'isUsedAsDuplex': 'pctUsedAsDuplex',
+    'isUsedAsTriplex': 'pctUsedAsTriplex',
+    'isUsedAsMultiBuildings1Lot': 'pctUsedAsMultiBuildings1Lot',
+    'isUsedAsAptUpTo30Units': 'pctUsedAsAptUpTo30Units',
+    'isUsedAsAptOver30Units': 'pctUsedAsAptOver30Units',
+    'isUsedAsCoopApt': 'pctUsedAsCoopApt',
+    'isUsedAsOtherMultifamily': 'pctUsedAsOtherMultifamily',
+    'mustUpzone': 'pctMustUpzone',
+    'willChange': 'pctWillChange'
+},
+                                       inplace=True)
+
+print(brookline_milton_stop_zone_data)
 
 # add in census data
 
+station_buffer_census_cumulative = gpd.read_file(
+    PREPROCESSED_DATA_PATH / 'station_buffer_census_cumulative.geojson')
+
+brookline_milton_stop_zone_census = brookline_milton_stop_zone_data.merge(
+    station_buffer_census_cumulative, on="stop_name")
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(
+    f"Time taken to compute brookline_milton_stop_zone_census {len(brookline_milton_stop_zone_census)} lines:",
+    elapsed_time, "seconds")
+
+brookline_milton_stop_zone_census.to_file(
+    PREPROCESSED_DATA_PATH / "brookline_milton_stop_zone_census.geojson",
+    driver='GeoJSON')
 
 # add in stop zone geographies
