33855614f68fa1026e50d2705590ba1b984805aa
amelia-b <amelia_b@mit.edu>
2024-05-09 11:16:04 -0400

    update file name reference and filenames to be lowercase

--- eda/split_parcel_data_by_stop.py
+++ eda/split_parcel_data_by_stop.py
@@ -9,13 +9,13 @@ augmented_parcels_all_stops = gpd.read_file(
     STATIC_SITE_DATA_PATH /
     f"brookline_milton_parcels_for_upzone_willchange_viz.geojson")
 
-dfs = [group for _, group in augmented_parcels_all_stops.groupby('stop_name')]
 
 for stop_name in augmented_parcels_all_stops["stop_name"].unique():
+    file_name = stop_name.lower().replace(' ', '_')
 
     df_for_stop = augmented_parcels_all_stops[
         augmented_parcels_all_stops["stop_name"] == stop_name]
     df_for_stop.to_file(
         STATIC_SITE_DATA_PATH /
-        f"parcels/per_station/upzone_willchange_parcels_{stop_name}.geojson",
+        f"parcels/per_station/upzone_willchange_parcels_{file_name}.geojson",
         driver='GeoJSON')
