15317897c219c2d84ab409265531f6101e67eb8b
amelia-b <amelia_b@mit.edu>
2024-05-09 11:00:55 -0400

    generate augmented parcel data for brookline, milton

---
+++ eda/split_parcel_data_by_stop.py
@@ -0,0 +1,21 @@
+import geopandas as gpd
+from pathlib import Path
+
+STATIC_SITE_DATA_PATH = Path(
+    "/Users/ameliabaum/Library/Mobile Documents/com~apple~CloudDocs/MIT/6.C35/6.C85_FP/code/static/data"
+)
+
+augmented_parcels_all_stops = gpd.read_file(
+    STATIC_SITE_DATA_PATH /
+    f"brookline_milton_parcels_for_upzone_willchange_viz.geojson")
+
+dfs = [group for _, group in augmented_parcels_all_stops.groupby('stop_name')]
+
+for stop_name in augmented_parcels_all_stops["stop_name"].unique():
+
+    df_for_stop = augmented_parcels_all_stops[
+        augmented_parcels_all_stops["stop_name"] == stop_name]
+    df_for_stop.to_file(
+        STATIC_SITE_DATA_PATH /
+        f"parcels/per_station/upzone_willchange_parcels_{stop_name}.geojson",
+        driver='GeoJSON')
