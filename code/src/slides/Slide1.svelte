d79dc4b998c45c272a60bd68084f3f990fd01cda
Riccardo Fiorista <fiorista@mit.edu>
2024-08-09 10:17:19 +0200

    Update Slide1.svelte

--- code/src/slides/Slide1.svelte
+++ code/src/slides/Slide1.svelte
@@ -7,7 +7,7 @@
     <div class="slide">
         <h1>MARGINAL OR MEANINGFUL?</h1>
         <h2>An immersive tool for exploring the impacts of the MBTA Communities Act</h2>
-        <p>- Amelia Baum, Riccardo Fiorista, Simone Peter, Ravi Tejwani -</p>
+        <p>- Amelia Baum, Riccardo Fiorista, and Simone Peter -</p>
         <div class="scroll-indicator"></div>
     </div>
 {/if}
